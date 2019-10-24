6dbabeaff7f6a0e4f4d0244245b4f5851ae1c712
Christoph Hube <hube@l3s.de>
2019-10-24 13:53:21 +0200

    updated sentiment classifier

--- evaluation/sent_classifier.py
+++ evaluation/sent_classifier.py
@@ -13,18 +13,18 @@ import torch
 import pickle
 
 '''
-    Set up the arguments and parse them.
+    Code for training and testing a word-level sentiment classifier based on positive and negative words.
 '''
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(
-        description='Use this script to train the oracl sentiment classifier.')
-    parser.add_argument('-e', '--emb', help='The path to the W2V embedding file.', required=False)
+        description='Use this script to train the oracle sentiment classifier.')
+    parser.add_argument('-e', '--emb', help='The path to the W2V embedding file.', required=True)
     parser.add_argument('-p', '--pos', help='The path to the positive words.', required=True)
     parser.add_argument('-n', '--neg', help='The path to the negative words.', required=True)
-    parser.add_argument('-d', '--dict', help='The path to the names dictionary.', required=True)
-    parser.add_argument('-o', '--out', help='The path to the output folder where we store the weights', required=True)
+    parser.add_argument('-ev', '--eval', help='The path to the list of words to be evaluated (e.g. names).', required=True)
+    parser.add_argument('-o', '--out', help='The path to the output folder where we store the weights.', required=True)
     parser.add_argument('-t', '--tag',
                         help='The tag with which we will assign the file names to distinguish the different embeddings.',
                         required=False, default='')
@@ -48,52 +48,53 @@ def vec(w, emb):
 
 def train_model(emb_file, pos_file, neg_file, out_dir, tag):
     tag = os.path.basename(emb_file) + '' + tag
+
     # read word embeddings as DataFrame
     emb = pd.read_table(emb_file, sep=' ', header=None, skiprows=1, index_col=0)
     indexes = emb.index.values
-
-    print (len(indexes))
 
     # prepare training data
     pos_words = read_word_lexicon(pos_file, indexes)
     neg_words = read_word_lexicon(neg_file, indexes)
 
+    # balance datasets
     if len(pos_words) > len(neg_words):
         random.shuffle(pos_words)
         pos_words = pos_words[:len(neg_words)]
     else:
         random.shuffle(neg_words)
         neg_words = neg_words[:len(pos_words)]
-
     words = pos_words + neg_words
+    print("Number of training examples (pos/neg words) per class: ", len(words))
 
-    print (len(words))
     # convert to word vectors
     word_vectors = [vec(w, emb) for w in words]
 
     labels = [1 for pos_word in pos_words] + [-1 for neg_word in neg_words]
     assert len(word_vectors) == len(labels)
-    train_X, test_X, train_y, test_y = train_test_split(word_vectors, labels, test_size=0.1, random_state=4)
+    train_X, test_X, train_y, test_y = train_test_split(word_vectors, labels, test_size=0.1, random_state=1)
 
-    clf = LogisticRegression(solver='liblinear', random_state=4)
+    # train and predict
+    clf = LogisticRegression(solver='liblinear', random_state=1)
     clf.fit(train_X, train_y)
     y_pred = clf.predict(test_X)
 
+    # save weights
     tensor_weight = torch.Tensor(clf.coef_)
     pickle.dump(tensor_weight, open(out_dir + '/' + tag + '_weights.pckl', 'wb'), pickle.HIGHEST_PROTOCOL)
-
     outstr = str(clf.intercept_) + '\t'
     for val in tensor_weight[0]:
         outstr += str(val.item()) + '\t'
     open(p.out + '/' + tag + '_weights.txt', 'wt').write(outstr)
-    print('Intercept: %.3f' % clf.intercept_)
+    # print('Intercept: %.3f' % clf.intercept_)
 
+    # save model
     pickle.dump(clf, open(out_dir + '/' + tag + '_model.clf', 'wb'), pickle.HIGHEST_PROTOCOL)
 
     # performance
-    print(accuracy_score(test_y, y_pred, normalize=True, sample_weight=None))
+    print("Accuracy: ", accuracy_score(test_y, y_pred, normalize=True, sample_weight=None))
     print(classification_report(test_y, y_pred, digits=4))
-    print(confusion_matrix(test_y, y_pred))
+    print("Confusion matrix: ", confusion_matrix(test_y, y_pred))
 
     return clf, emb
 
@@ -107,32 +108,34 @@ if __name__ == '__main__':
     elif p.flag == 'test':
         clf = pickle.load(open(p.classifier, 'rb'))
         emb = pd.read_table(p.emb, sep=' ', header=None, skiprows=1, index_col=0)
-
     print("---------------------------------------")
+
     # sentiment scores
-    eval_words = [e.strip().lower() for e in open(p.dict, 'rt').readlines()]
+    eval_words = [e.strip().lower() for e in open(p.eval, 'rt').readlines()]
     out_str = 'word\tPOS\tNEG\tdiff\n'
-    print('word\tPOS\tNEG\tdiff\n')
+    # print(out_str)
 
     avg_pos = 0
     avg_neg = 0
     avg_diff = 0
     counter = 0
     for word in eval_words:
+        # check if eval word exists in embeddings
         word_vec = vec(word, emb)
         if word_vec is None:
             continue
 
+        # predict class probabilities
         counter += 1
-        out_str += '%s\t%.3f\t%.3f\t%.3f\n' % (word, clf.predict_proba([word_vec])[:, 1], clf.predict_proba([word_vec])[:, 0], abs(clf.predict_proba([word_vec])[:, 1]-.5))
-
         avg_pos += clf.predict_proba([word_vec])[:, 1]
         avg_neg += clf.predict_proba([word_vec])[:, 0]
-        avg_diff += abs(clf.predict_proba([word_vec])[:, 1]-.5)
-        print ('%s\t%.3f\t%.3f\t%.3f' % (word, clf.predict_proba([word_vec])[:, 1], clf.predict_proba([word_vec])[:, 0], abs(clf.predict_proba([word_vec])[:, 1]-.5)))
+        avg_diff += abs(clf.predict_proba([word_vec])[:, 1] - .5)
+        out_str += '%s\t%.3f\t%.3f\t%.3f\n' % (word, avg_pos, avg_neg, avg_diff)
+        # print('%s\t%.3f\t%.3f\t%.3f' % (word, avg_pos, avg_neg, avg_diff))
 
+    # compute average
     out_str += '\nAVG\t%.3f\t%.3f\t%.3f\n' % (avg_pos/counter, avg_neg/counter, avg_diff/counter)
-    print ('\nAVG\t%.3f\t%.3f\t%.3f\n' % (avg_pos / counter, avg_neg / counter, avg_diff / counter))
+    print('\nAVG\t%.3f\t%.3f\t%.3f\n' % (avg_pos / counter, avg_neg / counter, avg_diff / counter))
 
     # output the results
     base = os.path.basename(p.emb) + '_results.txt'
